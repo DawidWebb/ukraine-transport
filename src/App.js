32aa7fd7f08b15c3763dc0409ab33c6848f6fe67
DawidWebb <tomasz.matras@developerweb.pl>
2022-03-30 13:10:51 +0200

    setup react-app, clear app tree, add basic dependiences, create: Redux store, stylesRoot.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { HashRouter as Router } from "react-router-dom";
+import { Fragment } from "react";
+// import { MainSwitch, Spinner, TaskInformation } from "./components";
+// import { MainHeader } from "./templates";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      {/* <MainHeader /> */}
+      <Fragment>{/* <MainSwitch /> */}</Fragment>
+      {/* <Spinner /> */}
+      {/* <TaskInformation /> */}
+    </Router>
   );
-}
+};
 
 export default App;
